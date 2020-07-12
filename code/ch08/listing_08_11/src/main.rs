93d8f18a75bdd5964c45f8a3d12d4492f128bbbb
jameskk <jamesmarva@users.noreply.github.com>
2020-07-12 20:54:20 +0800

    Update main.rs

--- code/ch08/listing_08_11/src/main.rs
+++ code/ch08/listing_08_11/src/main.rs
@@ -1,6 +1,8 @@
+#[warn(unused_variables)]
 fn main() {
     println!("Hello, world!");
     let mut s = String::new();
     s = "pppppppppppppppppppp".to_string();
     println!("{}", s);
+    let s = String::from("adfasdfasfd");
 }
