93f91ec257f168138d72aeb44498b2b088b58247
jameskk <jamesmarva@users.noreply.github.com>
2020-09-08 02:59:46 +0800

    10 & 4

--- code/ch04/clone_example/src/main.rs
+++ code/ch04/clone_example/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    // let s1 = String::from("hello");
+    // let s2 = s1.clone();
 
-    println!("s2 = {}", s2);
+    // println!("s2 = {}", s2);
 
-    
+    // let s1 = String::from("hello Rust.");
+    // let s2 = s1.clone();
+    // println!("s1 = {}", s1);
+    let i1:i32 = 1000;
+    let i2:i32 = i1.clone();
+    println!("i1 = {}", i1);
+    println!("i2 = {}", i2);
 }
--- code/ch04/listing_4_5/src/main.rs
+++ code/ch04/listing_4_5/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
    let s1 = String::from("hhhhhhhhhhhhhhh");
     let (s2, len) = calculate_length(s1);
-
     println!("length {}, s: {}", len, s2);
-
 }
 
 fn calculate_length(s: String) -> (String, usize) {
