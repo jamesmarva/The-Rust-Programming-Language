578287b60ffd650ae3796f2e7575e39723f56f2d
jameskk <jamesmarva@users.noreply.github.com>
2020-10-11 18:39:05 +0800

    新增代码

--- code/ch12/listing_12_19/src/main.rs
+++ code/ch12/listing_12_19/src/main.rs
@@ -1,3 +1,13 @@
+use listing_12_19::run;
+use std::env;
+use listing_12_19::Config;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let con = Config::new(&args).unwrap_or_else (|error| {
+        println!("{}", error);
+        process::exit(1);
+    });
+    run(con);
 }
