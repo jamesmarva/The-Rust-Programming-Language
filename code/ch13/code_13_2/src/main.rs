b87d6334e157123bb2c12031b3ec0ce55c328043
jameskk <jamesmarva@users.noreply.github.com>
2020-10-15 00:30:44 +0800

    update

--- code/ch13/code_13_2/src/main.rs
+++ code/ch13/code_13_2/src/main.rs
@@ -1,5 +1,13 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let simulated_user_specified_value = 10; 
     let simulated_random_number = 7; 
+}
 
+fn simlated_expensive_calculation(intensity: u32) ->u32 {
+    println!("calculating slowly.");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
--- code/ch13/code_13_4/src/main.rs
+++ code/ch13/code_13_4/src/main.rs
@@ -1,8 +1,31 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let simlated_user_specified_value = 10;
+    let simlated_random_number = 7;
+    generate_workout(simlated_user_specified_value, simlated_random_number);
 }
 
+fn simlated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly.");
+    thread::sleep(Duration::from_secs(1));
+    intensity
+}
 
-fn generate_workout() {
-    // let 
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simlated_expensive_calculation(intensity);
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            )
+        }
+    }
 }
