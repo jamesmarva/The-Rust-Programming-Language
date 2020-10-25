efe7c4a1f723ce7726f8c15065d6271a696bb935
jameskk <jamesmarva@users.noreply.github.com>
2020-10-26 03:04:49 +0800

    update ch15

--- code/ch15/code_15_9/src/main.rs
+++ code/ch15/code_15_9/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "aaa";
+    println!("{}", &s[0..1]);
+}
+
+struct MyBox<T> (T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
