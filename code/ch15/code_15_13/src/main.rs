331d5f34c5e39130066ce44ac1948bd6aa1d044f
jameskk <jamesmarva@users.noreply.github.com>
2020-10-30 08:03:00 +0800

    update ch15

--- code/ch15/code_15_13/src/main.rs
+++ code/ch15/code_15_13/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    
+    let m = MyBox::new(String::from("rust"));
+    hello(&(*(m.myDeref()))[..]);
+    hello(&(*m.myDeref())[..])
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+
+    fn myDeref(&self) -> &T {
+        &self.0
+    }
+}
+
+
+fn hello(s: &str) {
+    println!("{}", s);
 }
