13eeb8c2f127792023505442fe92be3d28a3dc58
jameskk <jamesmarva@users.noreply.github.com>
2020-10-31 08:42:06 +0800

    update chapter 15

--- code/ch15/code_15_2/src/main.rs
+++ code/ch15/code_15_2/src/main.rs
@@ -1,3 +1,8 @@
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+
 fn main() {
     println!("Hello, world!");
 }
--- code/ch15/code_15_3/src/main.rs
+++ code/ch15/code_15_3/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use crate::List::{Cons, Nil}; 
+
+enum List {
+    Cons(i32, List),
+    Nil,
 }
+
+fn main() {
+
+    let list = Cons(1, Cons(3, Nil));
+
+}
+
+
