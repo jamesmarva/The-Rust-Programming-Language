311c466b282d037d46de4e9b3b5022007c37bda4
jameskk <jamesmarva@users.noreply.github.com>
2020-11-05 08:31:45 +0800

    update ch15

--- code/ch15/code_15_24/src/main.rs
+++ code/ch15/code_15_24/src/main.rs
@@ -14,6 +14,15 @@ fn main() {
 
     let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
 
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+
+    let c = Cons(Rc::new(RefCell::new(7)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
     
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 
 }
