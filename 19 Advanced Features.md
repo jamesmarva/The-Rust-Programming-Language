b394a828c7a922e798ef38b3efc7f2d37c8a78d5
jameskk <jamesmarva@users.noreply.github.com>
2020-12-01 23:27:06 +0800

    update 19

---
+++ 19 Advanced Features.md
@@ -0,0 +1,18 @@
+- UnSafe Rust:
+- Advanced traits:
+- Advanced types:
+- Advanced function and closures:
+- Macros:
+
+# 1 不安全Rust （Unsafe Rust）
+
+
+## 1.1 Unsafe Superpowers
+
+- Dereference a raw pointer
+
+## 1.2 Dereferencing a Raw Pointer
+
+
+
+# 2 
--- code/ch18/code_18_29/src/main.rs
+++ code/ch18/code_18_29/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+
+    enum Message {
+        Hello {
+            id: i32,
+        }
+    }
+
+    let mut msg = Message::Hello{id: 5};
+
+    match msg {
+        Message::Hello{id: val @ 3..=6} => {
+            println!("id is in range: {}", val);
+        },
+        Message::Hello{id: 8..=12} => {
+            println!("id is in another range");
+        },
+        Message::Hello{id} => {
+            println!("Found some other, {}", id);
+        }
+    }
+
+    msg = Message::Hello{id: 15};
+
+    match msg {
+        Message::Hello{id: val @ 3..=6} => {
+            println!("id is in range: {}", val);
+        },
+        Message::Hello{id: 8..=12} => {
+            println!("id is in another range");
+        },
+        Message::Hello{id: id_val} => {
+            println!("Found some other, {}", id_val);
+        }
+    }
 }
