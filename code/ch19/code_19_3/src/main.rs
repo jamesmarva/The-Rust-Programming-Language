dc5f8fa0d916ac196d5c8724b63b5186883d26d8
jameskk <jamesmarva@163.com>
2021-01-05 13:41:44 +0800

    update chapter 19

--- code/ch19/code_19_3/src/main.rs
+++ code/ch19/code_19_3/src/main.rs
@@ -11,7 +11,13 @@ fn main() {
 
 
 fn demo_1() {
-    let mut num = 5;
-    let r1 = num as *const i32;
-    let r2 = mut num as *mut i32;
+    let mut n = 5;
+    let r1 = &n as *const i32;
+    let r2 = &mut n as *mut i32;
+}
+
+fn demo_2() {
+    let mut n = 4;
+    let r1 = &n as *const i32;
+    let r2 = &mut n as *mut i32;
 }
