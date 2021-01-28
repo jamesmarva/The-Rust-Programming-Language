2a12b6509bb89fa7bbd8e690e0f2e2a112036aa2
jameskk <jamesmarva@163.com>
2021-01-28 21:08:15 +0800

    Update main.rs

--- code/ch19/code_19_12/src/main.rs
+++ code/ch19/code_19_12/src/main.rs
@@ -1,2 +1,23 @@
 fn main() {
+
+
+}
+
+struct Counter {
+    count:u32
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.count)
+    }
+}
+
+
+pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
 }
